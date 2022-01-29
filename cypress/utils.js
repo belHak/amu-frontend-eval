6b3251d41a9104fdf7412e3a1cdd9eac08049bf4
belHak <hak721@outlook.fr>
2022-01-29 23:20:21 +0100

    init supabase attributes

--- cypress/utils.js
+++ cypress/utils.js
@@ -1,9 +1,9 @@
 // L'URL sur laquelle votre application web est visitable (à changer si nécessaire)
 export const BASE_URL = "http://localhost:4200/";
 // L'URL de l'API Supabase à mettre à jour absolument
-export const API_URL = "https://IDENTIFANT_SUPABASE.supabase.co/rest/v1";
+export const API_URL = "https://zkalavdguunjgydiufrn.supabase.co/rest/v1";
 // La clé d'API de votre compte Supabase à mettre à jour absolument
-export const API_KEY = "CLE_API_SUPABASE";
+export const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzQ2ODE5MywiZXhwIjoxOTU5MDQ0MTkzfQ.DM7vIfU9wNFnGZggDs9xVorvsNo-1WLGtNQTsrMp_Jc";
 
 /**
  * Petite fonction utilitaire qui permet de supprimer tout ce qui se trouve dans les tables customers et invoices
