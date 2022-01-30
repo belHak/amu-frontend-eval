6f22f3d011d05cbde4e07be22f7dd7107e484098
belHak <hak721@outlook.fr>
2022-01-30 12:52:22 +0100

    app : update app modules + add routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerListComponent} from "./component/customer-list/customer-list.component";
+import {CustomerCreateComponent} from "./component/customer-create/customer-create.component";
+import {CustomerDetailsComponent} from "./component/customer-details/customer-details.component";
+import {InvoiceAddComponent} from "./component/invoice-add/invoice-add.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CustomerListComponent },
+  { path: 'create', component: CustomerCreateComponent },
+  { path: ':id', component: CustomerDetailsComponent },
+  { path: ':idcustomer/invoices/add', component: InvoiceAddComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerCreateComponent } from './component/customer-create/customer-create.component';
+import { CustomerDetailsComponent } from './component/customer-details/customer-details.component';
+import { CustomerListComponent } from './component/customer-list/customer-list.component';
+import { InvoiceAddComponent } from './component/invoice-add/invoice-add.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {ApiService} from "./service/api.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerCreateComponent,
+    CustomerDetailsComponent,
+    CustomerListComponent,
+    InvoiceAddComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
